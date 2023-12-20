2fb4196d0be96726c0528cb933f2c827fef4e2a6
Arjun <arjunpks95@gmail.com>
2023-12-20 14:05:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -26,28 +26,72 @@ def download_en_core_web_sm():
     
 nlp = spacy.load("en_core_web_sm")
 # Load spaCy model
-model_name = 'en_core_web_lg'
-nlp = spacy.load(model_name)
+ 
 
-# Function to check if the article is in English
 def is_english(text):
     lang, _ = classify(text)
     return lang == 'en'
 
-# Function to get full article using newspaper3k and perform NLP
 def get_full_article_with_nlp(url):
-    # ... (same as before)
+    article = GNews().get_full_article(url)
 
-# Function to process the article
+    try:
+        article.download()
+        article.parse()
+        article.nlp()
+    except Exception as e:
+        print(f"Error performing NLP on the article: {e}")
+
+    return {
+        'title': article.title,
+        'text': article.text,
+        'authors': article.authors,
+        'summary': article.summary,
+        'keywords': article.keywords,
+    }
+
 def process_article(article):
-    # ... (same as before)
+    source = article.get('source', 'N/A')
+    if source == 'N/A':
+        url = article.get('url', '')
+        if url:
+            source_from_url = url.split('/')[2]
+            source = source_from_url if source_from_url else 'N/A'
+
+    if is_english(article.get('title', '')):
+        title = article.get('title', 'N/A')
+
+        if ' ... - ' in title:
+            title_parts = title.split(' ... - ')
+        elif ' - ' in title:
+            title_parts = title.split(' - ')
+        else:
+            title_parts = [title]
+
+        article_source = title_parts[1].strip() if len(title_parts) > 1 else 'N/A'
+
+        result = {
+            'Title': article.get('title', 'N/A'),
+            'Source': source,
+            'Article_Source': article_source,
+            'URL': article.get('url', 'N/A'),
+        }
+
+        try:
+            full_article = get_full_article_with_nlp(article.get('url'))
+            result.update(full_article)
+        except Exception as e:
+            print(f"Error processing article: {e}")
+
+        return result
+    else:
+        return None
 
 # Sample data loading
 data = df
 stop_words_list = list(ENGLISH_STOP_WORDS)
 stop_words = list(ENGLISH_STOP_WORDS)
 
-# Data preprocessing for clustering
 data['text'] = data['text'].fillna('')
 data = data[~data['text'].str.contains("Save my User ID and Password")]
 data = data.dropna(subset=['text'])
@@ -57,52 +101,36 @@ data['text'] = data['text'].apply(lambda x: ' '.join([word.lower() for word in w
 st.title("News Scraper and Analyzer")
 st.sidebar.header("Search Parameters")
 
-# User input
 query = st.sidebar.text_input("Enter Keywords (separated by space)", 'datacentre cloud')
 start_date = st.sidebar.date_input("Enter Start Date", datetime(2023, 11, 1))
 end_date = st.sidebar.date_input("Enter End Date", datetime(2023, 12, 30))
 
-# Set the language filter
 GNews.language = 'en'
-
-# Initialize GNews object with query parameters
 google_news = GNews(country='US', max_results=100)
 
-# Search button
 if st.sidebar.button("Search"):
     with st.spinner("Please wait while scraping the web..."):
-        # Set the start and end dates based on UI input
         google_news.start_date = start_date
         google_news.end_date = end_date
 
-        # Get news results
         news_results = google_news.get_news(query)
 
-        # Parallel processing using joblib
         results_list = Parallel(n_jobs=-1)(delayed(process_article)(article) for article in news_results)
-
-        # Filter out None values (articles not in English)
         results_list = [result for result in results_list if result is not None]
-
-        # Create a DataFrame from the list of results
         df = pd.DataFrame(results_list)
 
-        # Data processing for clustering
         data['text'] = data['text'].fillna('')
         data = data[~data['text'].str.contains("Save my User ID and Password")]
         data = data.dropna(subset=['text'])
         data['text'] = data['text'].apply(lambda x: ' '.join([word.lower() for word in word_tokenize(x) if word.isalpha() and word.lower() not in stop_words]))
 
-        # Text vectorization using TF-IDF
         vectorizer = TfidfVectorizer(max_features=1000, stop_words=stop_words_list)
         X = vectorizer.fit_transform(data['text'])
 
-        # Clustering using K-Means
-        num_clusters = 5  # You can adjust this number based on your dataset
+        num_clusters = 5
         kmeans = KMeans(n_clusters=num_clusters, random_state=42)
         df['cluster'] = kmeans.fit_predict(X)
 
-        # Download button for the entire DataFrame
         entire_df_output = BytesIO()
         df.to_csv(entire_df_output, index=False)
         st.sidebar.download_button(
@@ -114,18 +142,15 @@ if st.sidebar.button("Search"):
             help="Click to download the entire DataFrame"
         )
 
-# Display results
 st.subheader("Results")
 st.dataframe(df)
 
-# Display cluster summaries
 st.subheader("Cluster Summaries")
 for i, (summary, keywords) in enumerate(zip(cluster_summaries, cluster_keywords), 1):
     st.write(f"\nCluster {i} Summary:")
     st.write(f"Cluster Summary: {summary}")
     st.write(f"Cluster Keywords: {', '.join(keywords)}")
 
-# Download button for the cluster summaries
 summary_doc = Document()
 for i, (summary, keywords) in enumerate(zip(cluster_summaries, cluster_keywords), 1):
     summary_doc.add_heading(f'Cluster {i} Summary', level=2)
@@ -133,7 +158,6 @@ for i, (summary, keywords) in enumerate(zip(cluster_summaries, cluster_keywords)
     summary_doc.add_heading(f'Cluster {i} Keywords', level=2)
     summary_doc.add_paragraph(', '.join(keywords))
 
-# Save the document
 output_summary = BytesIO()
 summary_doc.save(output_summary)
 st.sidebar.download_button(
